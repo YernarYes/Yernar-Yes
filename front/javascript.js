9dbd58f56dbb76a3636753df362a979fc36544b6
YernarYes <61062657+YernarYes@users.noreply.github.com>
2022-02-21 08:19:03 +0600

    Add files via upload

---
+++ front/javascript.js
@@ -0,0 +1,46 @@
+/*
+$("button").click(function(){
+  var div = $("div");
+  div.animate({left: '100px'}, "slow");
+  div.animate({fontSize: '3em'}, "slow");
+});
+*/
+alert("Hello dear student!");
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+const labels = document.querySelectorAll('.form-control label')
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+    .split('')
+    .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+    .join('')
+})
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+
+
+function bigImg(x) {
+  x.style.height = "64px";
+  x.style.width = "64px";
+}
+
+function normalImg(x) {
+  x.style.height = "32px";
+  x.style.width = "32px";
